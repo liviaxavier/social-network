75a5785c4d5c9b982525f26f371c6d07c3896ff7
Livia Xavier <liviaxavier.co@gmail.com>
2024-07-05 15:21:03 -0300

    chore: set firebase env

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responseive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responseive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responseive/web_screen_layout.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+    await Firebase.initializeApp(options: const FirebaseOptions(
+        apiKey: "AIzaSyCT5HGZKBjMnFWnGZbaV53gFctyIYpiRUc", 
+        appId: "1:694024476508:web:84e104bf686de34429d399", 
+        messagingSenderId: "694024476508", 
+        projectId: "instagram-clone-7fcb5",
+        storageBucket: "instagram-clone-7fcb5.appspot.com"
+        ),);
+  } else {
+    await Firebase.initializeApp(); // pra mobile
+  }
+    runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
