9a2fd80f17619626627687ccb810913ac8b1ca7b
Livia Xavier <liviaxavier.co@gmail.com>
2024-07-22 12:41:34 -0300

    feat: return username at mobile screen

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,10 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
 
   @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  String usernmae = "";
+
+  @override
+  void initState() {
+    super.initState();
+    getUsername();
+  }
+
+  void getUsername() async {
+    DocumentSnapshot snap = await FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser!.uid).get();
+    setState(() {
+      usernmae = (snap.data() as Map<String, dynamic>)['username'];
+    });
+  }
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Mobile Screen'));
+    return Scaffold(body: Center(child: Text(usernmae)));
   }
 }
